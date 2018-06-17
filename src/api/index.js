5b793aba8e49aa1605f8105fec75105ca20a60de
Ivar Nilsen <post@ivarnilsen.com>
2018-06-17 13:58:37 +0200

    fetch is apparently polyfilled by cra

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,3 @@
-import 'whatwg-fetch';
-
 const LEAGUE_URL = 'https://www.vg.no/spesial/2018/fotball-vm/api/hent_liga.php?liga=56e56251c0ff2001546b6fb768101933';
 const MATCH_URL = (id) => `https://redutv-api.vg.no/sports/fotball-vm-2018/kamp/${id}`;
 
