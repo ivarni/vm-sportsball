8267804414846d56966fa10d92822396bb4d4326
Ivar Nilsen <post@ivarnilsen.com>
2018-06-17 14:52:04 +0200

    proxy requests

---
+++ server.js
@@ -0,0 +1,30 @@
+require('isomorphic-fetch');
+
+const path = require('path');
+const express = require('express');
+
+const LEAGUE_URL = 'https://www.vg.no/spesial/2018/fotball-vm/api/hent_liga.php?liga=56e56251c0ff2001546b6fb768101933';
+const MATCH_URL = (id) => `https://redutv-api.vg.no/sports/fotball-vm-2018/kamp/${id}`;
+
+const app = express();
+
+const root = path.join(__dirname, 'build');
+
+app.use('/api/league', (req, res) => {
+    fetch(LEAGUE_URL)
+        .then(resp => resp.json())
+        .then(json => res.json(json));
+});
+
+app.use('/api/match/:id', (req, res) => {
+    fetch(MATCH_URL(req.params.id))
+        .then(resp => resp.json())
+        .then(json => res.json(json));
+});
+
+app.use(express.static(root));
+
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'), () => {
+    console.log(`App listening on ${app.get('port')}`);
+})
--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
-const LEAGUE_URL = 'https://www.vg.no/spesial/2018/fotball-vm/api/hent_liga.php?liga=56e56251c0ff2001546b6fb768101933';
-const MATCH_URL = (id) => `https://redutv-api.vg.no/sports/fotball-vm-2018/kamp/${id}`;
+const LEAGUE_URL = '/api/league';
+const MATCH_URL = (id) => `/api/match/${id}`;
 
 export const getLeague = () =>
     fetch(LEAGUE_URL).then(resp => resp.json());
