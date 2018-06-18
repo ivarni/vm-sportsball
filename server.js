80cd13581eb83fb84365446a3f8ddb279b761aa4
Ivar Nilsen <post@ivarnilsen.com>
2018-06-18 17:53:57 +0200

    quality. delivered

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 const LEAGUE_URL = 'https://www.vg.no/spesial/2018/fotball-vm/api/hent_liga.php?liga=56e56251c0ff2001546b6fb768101933';
 const MATCH_URL = (id) => `https://redutv-api.vg.no/sports/fotball-vm-2018/kamp/${id}`;
-const GUESS = (id) => `https://www.vg.no/spesial/2018/fotball-vm/api/hent_tipping.php?id_public=${id}`;
+const GUESS_URL = (id) => `https://www.vg.no/spesial/2018/fotball-vm/api/hent_tipping.php?id_public=${id}`;
 const ALL_MATCHES_URL = 'https://redutv-api.vg.no/sports/fotball-vm-2018/kampene'
 
 const app = express();
@@ -29,7 +29,6 @@ app.use('/api/match/:id', (req, res) => {
         .then(resp => resp.json())
         .then(json => res.json(json));
 });
-
 
 app.use('/api/guess/:id', (req, res) => {
     fetch(GUESS_URL(req.params.id))
